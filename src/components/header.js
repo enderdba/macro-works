bf9072bb00b8a76cbb1b0fe00045386d2fc108e1
enderdba <ender.bohorquez@leniolabs.com>
2019-05-26 12:19:05 -0400

    Added logo header and changes on colors

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,13 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-
+import logo from "../images/macro_calc_header.png"
 const linkStyle = {
   color: `white`,
   padding: '1em',
   fontSize: '12pt',
+  border:'2px solid gray',
   margin: "1em",
-  border: "2px solid black",
   backgroundColor: 'pink',
   textDecoration: `none`,
 }
@@ -15,10 +15,11 @@ const Header = ({ siteTitle }) => (
   <header
     style={{ textAlign: "center", marginBottom: '1em' }}
   >
-    <h1 style={{ margin: 0 }}>
+    <img src={logo}></img>
+    <h1 className="section" style={{ margin: '0px auto' , maxWidth: '960px', padding: '1em'}}>
       <Link
         to="/"
-        style={{ ...linkStyle, backgroundColor: "#D8A3C1" }}
+        style={{ ...linkStyle, backgroundColor: "#ff99d6" }}
       >
         Female Calculator
         </Link>
--- src/components/slidebar.js
+++ src/components/slidebar.js
@@ -42,7 +42,7 @@ function Handle({ // your handle component
         textAlign: 'center',
         cursor: 'pointer',
         borderRadius: '50%',
-        backgroundColor: '#D8A3C1',
+        backgroundColor: '#FF66C4',
         color: '#333',
       }}
       {...getHandleProps(id)}
@@ -59,7 +59,7 @@ function Track({ source, target, getTrackProps }) {
         height: 10,
         zIndex: 1,
         marginTop: 35,
-        backgroundColor: '#FFD8EB',
+        backgroundColor: '#ff99d6',
         borderRadius: 5,
         cursor: 'pointer',
         left: `${source.percent}%`,
